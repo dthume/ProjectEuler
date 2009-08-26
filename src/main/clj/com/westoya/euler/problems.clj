ee4ef815d0b0387e58494f3a04beffae90a57235
David Hume <david_hume@bfs-ps-6.lagan.com>
2009-08-26 18:00:18 +0100

    remembered to git add file...

--- src/main/clj/com/westoya/euler/problems.clj
+++ src/main/clj/com/westoya/euler/problems.clj
@@ -2,12 +2,24 @@
 
 (defn natural-numbers [] (iterate inc 0))
 
-(def *natural-numbers* (natural-numbers))
+(defn fibonacci-sequence
+  ([]
+     (fibonacci-sequence 0 1))
+  ([a b]
+     (lazy-seq
+       (cons a (fibonacci-sequence b (+ a b))))))
 
 (defn problem1
   []
-  (reduce +
-	  (filter #(or (= 0 (mod %1 3))
-		       (= 0 (mod %1 5)))
-		  (take 1000 *natural-numbers*))))
+  (apply +
+	 (filter #(or (= 0 (mod %1 3))
+		      (= 0 (mod %1 5)))
+		 (take 1000 (natural-numbers)))))
+
+(defn problem2
+  []
+  (apply +
+	 (take-while
+	  #(< % 4000000)
+	  (filter even? (fibonacci-sequence)))))
 
