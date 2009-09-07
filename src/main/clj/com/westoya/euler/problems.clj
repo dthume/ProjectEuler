fd3eda28a431e7a8016d1f52317286969e272d46
David Thomas Hume <dthume@gmail.com>
2009-09-07 20:14:07 +0100

    Tweaked problem 24 solution to return output in usual form (i.e. matching what is displayed on the euler page)

--- src/main/clj/com/westoya/euler/problems.clj
+++ src/main/clj/com/westoya/euler/problems.clj
@@ -365,7 +365,7 @@ there to the bottom right corner?"
   "What is the millionth lexicographic permutation of the digits
 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9?"
   []
-  (nth (lex-permutations (range 10)) 999999))
+  (apply str (nth (lex-permutations (range 10)) 999999)))
 
 (defn problem25
   "What is the first term in the Fibonacci sequence to contain 1000 digits?"
