2d6713988feb98c02b055881a1dafe69bd043760
David Thomas Hume <dthume@gmail.com>
2009-09-07 20:14:42 +0100

    Remembered to add string-utils...

---
+++ src/main/clj/com/westoya/euler/string_utils.clj
@@ -0,0 +1,31 @@
+(ns com.westoya.euler.string-utils)
+
+(defn char-map
+  ""
+  [s f]
+  (into {} (map #(vector %1 (f %1)) s)))
+
+(def *digit-cache* {\0 0, \1 1, \2 2, \3 3, \4 4, \5 5, \6 6, \7 7, \8 8, \9 9})
+
+(defn parse-digit
+  "Parses a digit from character c"
+  [c]
+  (if-let [d (get *digit-cache* c)]
+    d
+    (Integer/parseInt (.toString c))))
+
+(defn digits
+  "Returns a sequence of the digits comprising n"
+  [n]
+  (map parse-digit (.toString n)))
+
+(defn reverse-string
+  "Reverses s"
+  [s]
+  (.toString (.reverse (StringBuilder. (str s)))))
+
+(defn palindrome?
+  "Returns true if the string representation of s is palindromic"
+  [s]
+  (let [s (str s)]
+    (= s (reverse-string s))))
